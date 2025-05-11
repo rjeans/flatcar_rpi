3ce853b6e15af0aa4f004bdd4e75fd91c71e2128
Richard Jeans <rich@jeansy.org>
2025-05-11 11:34:53 +0200

    updated

--- pwm-bcm2835-acpi.c
+++ pwm-bcm2835-acpi.c
@@ -171,10 +171,10 @@ static int bcm2835_pwm_apply(struct pwm_chip *chip, struct pwm_device *pwm,
 		ctrl_val &= ~(PWM_ENABLE << PWM_CONTROL_SHIFT(pwm->hwpwm));
 
 
-	dev_info(pc->dev, "PERIOD register (0x%x): %llu ns -> %u cycles\n",
+	dev_info(pc->dev, "PERIOD register (0x%x): %llu ns -> %llu cycles\n",
 		 PERIOD(pwm->hwpwm), state->period, period_cycles);
 
-    dev_info(pc->dev, "DUTY register (0x%x): %llu ns -> %u cycles\n",
+    dev_info(pc->dev, "DUTY register (0x%x): %llu ns -> %llu cycles\n",
          DUTY(pwm->hwpwm), state->duty_cycle, duty_val);
 
     dev_info(pc->dev, "PWM_CONTROL before write: 0x%08x\n", ctrl_val);
@@ -265,7 +265,7 @@ if (IS_ERR(pc->clk)) {
 	pc->chip.dev->of_node = NULL;
 
 
-			ret = pinctrl_register_mappings(bcm2835_pwm_map, ARRAY_SIZE(bcm2835_pwm_map));
+	ret = pinctrl_register_mappings(bcm2835_pwm_map, ARRAY_SIZE(bcm2835_pwm_map));
 	if (ret)
 		dev_warn(&pdev->dev, "Failed to register pinctrl mappings: %d\n", ret);
 
