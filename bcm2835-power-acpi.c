9195ecfe0fef9ca22c8a57bdb69638524be06c27
Richard Jeans <rich@jeansy.org>
2025-05-15 21:41:10 +0200

    Updated

--- bcm2835-power-acpi.c
+++ bcm2835-power-acpi.c
@@ -13,7 +13,8 @@
 #include <linux/acpi.h>
 #include <linux/fwnode.h>
 #include <linux/mailbox_controller.h>
-#include <linux/genpd.h> 
+#include <linux/fwnode.h>
+
 
 // External reference to mailbox channel
 extern struct mbox_chan *rpi_mbox_chan0;
@@ -190,7 +191,8 @@ if (ret) {
 
 // Register this power domain as a provider for ACPI/firmware consumers
 dev_info(dev, "Registering power domain '%s' with genpd\n", rpd->name);
-ret = dev_pm_genpd_add(dev, &rpd->genpd);
+ret = fwnode_add_power_domain(dev_fwnode(dev), &rpd->genpd);
+
 if (ret) {
 	dev_err(dev, "Failed to register genpd with firmware: %d\n", ret);
 	pm_genpd_remove(&rpd->genpd);
