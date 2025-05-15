3733249b54021160b9db2898548a6e2edc49d803
Richard Jeans <rich@jeansy.org>
2025-05-15 21:19:17 +0200

    Updated

--- pwm-bcm2835-acpi.c
+++ pwm-bcm2835-acpi.c
@@ -9,6 +9,7 @@
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/pwm.h>
+#include <linux/pm_domain.h>
 #include <linux/acpi.h>
 #include <linux/pinctrl/consumer.h> 
 #include <linux/pinctrl/machine.h>
