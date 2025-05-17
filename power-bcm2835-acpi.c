27c5dcac9d77b01fdd0aa5762ae459b920be59db
Richard Jeans <rich@jeansy.org>
2025-05-17 08:53:54 +0200

    Updated

--- power-bcm2835-acpi.c
+++ power-bcm2835-acpi.c
@@ -138,7 +138,6 @@ static int rpi_power_probe(struct platform_device *pdev)
 	rpd->mbox_client.dev = dev;
 	rpd->mbox_client.tx_block = true;
 	rpd->mbox_client.knows_txdone = true;
-	rpd->mbox_client.txdone_irq = true;
 
 	rpd->mbox_client.tx_done = rpi_power_tx_done;
 	pr_info("Requesting mailbox channel...\n");
