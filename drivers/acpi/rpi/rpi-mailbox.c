e5ce87718e94e3543950faf12587c20c514ef3dd
Richard Jeans <rich@jeansy.org>
2025-05-24 21:52:10 +0200

    Updated

--- drivers/acpi/rpi/rpi-mailbox.c
+++ drivers/acpi/rpi/rpi-mailbox.c
@@ -1,4 +1,23 @@
 // SPDX-License-Identifier: GPL-2.0
+/*
+ * rpi-mailbox.c - ACPI adaptation of the BCM2835 mailbox driver
+ *
+ * Copyright (C) 2010,2015 Broadcom
+ * Copyright (C) 2013-2014 Lubomir Rintel
+ * Copyright (C) 2013 Craig McGeachie
+ * Copyright (C) 2023 Richard Jeans <rich@jeansy.org>
+ *
+ * This driver is based on the original code from drivers/mailbox/bcm2835-mailbox.c.
+ * Parts of the driver are derived from:
+ *  - arch/arm/mach-bcm2708/vcio.c by Gray Girling, obtained from branch
+ *    "rpi-3.6.y" of git://github.com/raspberrypi/linux.git
+ *  - drivers/mailbox/bcm2835-ipc.c by Lubomir Rintel at
+ *    https://github.com/hackerspace/rpi-linux/blob/lr-raspberry-pi/drivers/
+ *    mailbox/bcm2835-ipc.c
+ *  - Documentation available at:
+ *    https://github.com/raspberrypi/firmware/wiki/Mailbox-property-interface
+ */
+
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/io.h>
@@ -42,7 +61,7 @@
 
 #define BCM2835_MAX_CHANNELS     16
 
-struct bcm2835_mbox {
+struct rpi_mbox {
     void __iomem *regs;
     struct mbox_controller controller;
     struct device *dev;
@@ -53,13 +72,13 @@ struct bcm2835_mbox {
 };
 
 
-struct bcm2835_mbox *bcm2835_mbox_global;
+struct rpi_mbox *rpi_mbox_global;
 
 #define RPI_MBOX_CHAN_FIRMWARE 8
 
-struct mbox_chan *bcm2835_mbox_request_firmware_channel(struct mbox_client *cl)
+struct mbox_chan *rpi_mbox_request_firmware_channel(struct mbox_client *cl)
 {
-	struct bcm2835_mbox *mbox = bcm2835_mbox_global;
+	struct rpi_mbox *mbox = rpi_mbox_global;
 	struct mbox_chan *chan;
 	int ret;
 
@@ -83,41 +102,44 @@ struct mbox_chan *bcm2835_mbox_request_firmware_channel(struct mbox_client *cl)
 
 	return chan;
 }
-EXPORT_SYMBOL_GPL(bcm2835_mbox_request_firmware_channel);
+EXPORT_SYMBOL_GPL(rpi_mbox_request_firmware_channel);
 
 
 
-struct mbox_chan *bcm2835_mbox_request_channel(struct mbox_client *cl)
+struct mbox_chan *rpi_mbox_request_channel(struct mbox_client *cl)
 {
-    struct mbox_chan *chan;
-    int i, ret;
+	struct mbox_chan *chan;
+	int i, ret;
 
-    if (!cl || !bcm2835_mbox_global)
-        return ERR_PTR(-ENODEV);
+	// Ensure the global mailbox is initialized
+	if (!cl || !rpi_mbox_global)
+		return ERR_PTR(-ENODEV);
 
-    for (i = 0; i < bcm2835_mbox_global->controller.num_chans; i++) {
-        if (i == RPI_MBOX_CHAN_FIRMWARE)
-                continue;  // Reserved for firmware
-        chan = &bcm2835_mbox_global->chans[i];
+	// Iterate through available channels to find a free one
+	for (i = 0; i < rpi_mbox_global->controller.num_chans; i++) {
+		if (i == RPI_MBOX_CHAN_FIRMWARE) // Skip reserved firmware channel
+			continue;
 
-        if (!chan->cl) {
-            ret = mbox_bind_client(chan, cl);
-            if (ret) {
-                pr_err("Failed to bind mailbox client: %d\n", ret);
-                return ERR_PTR(ret);
-            }
+		chan = &rpi_mbox_global->chans[i];
+		if (!chan->cl) {
+			ret = mbox_bind_client(chan, cl);
+			if (ret) {
+				pr_err("Failed to bind mailbox client: %d\n", ret);
+				return ERR_PTR(ret);
+			}
 
-            init_completion(&bcm2835_mbox_global->tx_completions[i]);
-            chan->mbox = &bcm2835_mbox_global->controller;
-            return chan;
-        }
-    }
+			init_completion(&rpi_mbox_global->tx_completions[i]);
+			chan->mbox = &rpi_mbox_global->controller;
+			return chan;
+		}
+	}
 
-    return ERR_PTR(-EBUSY);  // No free channel
+	// No free channel found
+	return ERR_PTR(-EBUSY);
 }
-EXPORT_SYMBOL_GPL(bcm2835_mbox_request_channel);
+EXPORT_SYMBOL_GPL(rpi_mbox_request_channel);
 
-int bcm2835_mbox_free_channel(struct mbox_chan *chan)
+int rpi_mbox_free_channel(struct mbox_chan *chan)
 {
 	if (!chan)
 		return -EINVAL;
@@ -129,11 +151,11 @@ int bcm2835_mbox_free_channel(struct mbox_chan *chan)
 
 	return 0;
 }
-EXPORT_SYMBOL_GPL(bcm2835_mbox_free_channel);
+EXPORT_SYMBOL_GPL(rpi_mbox_free_channel);
 
-static int bcm2835_send_data(struct mbox_chan *chan, void *data)
+static int rpi_mbox_send_data(struct mbox_chan *chan, void *data)
 {
-	struct bcm2835_mbox *mbox = container_of(chan->mbox, struct bcm2835_mbox, controller);
+	struct rpi_mbox *mbox = container_of(chan->mbox, struct rpi_mbox, controller);
 	u32 msg = *(u32 *)data;
 
 	spin_lock(&mbox->lock);
@@ -143,9 +165,9 @@ static int bcm2835_send_data(struct mbox_chan *chan, void *data)
 	return 0;
 }
 
-static bool bcm2835_last_tx_done(struct mbox_chan *chan)
+static bool rpi_mbox_last_tx_done(struct mbox_chan *chan)
 {
-	struct bcm2835_mbox *mbox = container_of(chan->mbox, struct bcm2835_mbox, controller);
+	struct rpi_mbox *mbox = container_of(chan->mbox, struct rpi_mbox, controller);
 	bool ret;
 
 	spin_lock(&mbox->lock);
@@ -155,9 +177,9 @@ static bool bcm2835_last_tx_done(struct mbox_chan *chan)
 	return ret;
 }
 
-static int bcm2835_startup(struct mbox_chan *chan)
+static int rpi_mbox_startup(struct mbox_chan *chan)
 {
-	struct bcm2835_mbox *mbox = container_of(chan->mbox, struct bcm2835_mbox, controller);
+	struct rpi_mbox *mbox = container_of(chan->mbox, struct rpi_mbox, controller);
 
 	/* Enable the interrupt on data reception */
 	writel(ARM_MC_IHAVEDATAIRQEN, mbox->regs + MAIL0_CNF);
@@ -165,35 +187,38 @@ static int bcm2835_startup(struct mbox_chan *chan)
 	return 0;
 }
 
-static void bcm2835_shutdown(struct mbox_chan *chan)
+static void rpi_mbox_shutdown(struct mbox_chan *chan)
 {
-	struct bcm2835_mbox *mbox;
+	struct rpi_mbox *mbox;
 
-	mbox = container_of(chan->mbox, struct bcm2835_mbox, controller);
+	mbox = container_of(chan->mbox, struct rpi_mbox, controller);
 }
 
-static irqreturn_t bcm2835_mbox_irq(int irq, void *dev_id)
+static irqreturn_t rpi_mbox_irq(int irq, void *dev_id)
 {
-	struct bcm2835_mbox *mbox = dev_id;
+	struct rpi_mbox *mbox = dev_id;
 	struct device *dev = mbox->controller.dev;
 	irqreturn_t handled = IRQ_NONE;
 
+	// Process all pending messages
 	while (!(readl(mbox->regs + MAIL0_STA) & ARM_MS_EMPTY)) {
 		u32 msg = readl(mbox->regs + MAIL0_RD);
 		u32 chan_index = msg & 0xf;
-		
 
+		// Validate channel index
 		if (chan_index >= BCM2835_MAX_CHANNELS) {
 			dev_warn(dev, "Invalid channel index %u in IRQ msg 0x%08X\n", chan_index, msg);
 			continue;
 		}
 
+		// Get the channel and ensure it is bound
 		struct mbox_chan *chan = &mbox->chans[chan_index];
 		if (!chan->cl || !chan->cl->rx_callback) {
 			dev_warn(dev, "Unbound mailbox channel %u (msg=0x%08X), skipping\n", chan_index, msg);
 			continue;
 		}
 
+		// Dispatch the message to the client
 		mbox_chan_received_data(chan, &msg);
 		handled = IRQ_HANDLED;
 	}
@@ -201,81 +226,97 @@ static irqreturn_t bcm2835_mbox_irq(int irq, void *dev_id)
 	return handled;
 }
 
-
-static const struct mbox_chan_ops bcm2835_chan_ops = {
-    .send_data     = bcm2835_send_data,
-    .startup       = bcm2835_startup,
-    .shutdown      = bcm2835_shutdown,
-    .last_tx_done  = bcm2835_last_tx_done,
+static const struct mbox_chan_ops rpi_mbox_chan_ops = {
+	.send_data     = rpi_mbox_send_data,
+	.startup       = rpi_mbox_startup,
+	.shutdown      = rpi_mbox_shutdown,
+	.last_tx_done  = rpi_mbox_last_tx_done,
 };
 
-
-
-
-
-
-
-
-static int bcm2835_mbox_probe(struct platform_device *pdev)
+static int rpi_mbox_probe(struct platform_device *pdev)
 {
-    struct bcm2835_mbox *mbox;
-    struct resource *res;
-    int ret;
+	struct rpi_mbox *mbox;
+	struct resource *res;
+	int ret;
 
-    mbox = devm_kzalloc(&pdev->dev, sizeof(*mbox), GFP_KERNEL);
-    if (!mbox)
-        return -ENOMEM;
+	// Log the start of the probe function
+	dev_info(&pdev->dev, "Probing rpi-mailbox device\n");
 
-    platform_set_drvdata(pdev, mbox);
-    mbox->dev = &pdev->dev;
-    bcm2835_mbox_global = mbox;
+	// Allocate memory for the mailbox structure
+	mbox = devm_kzalloc(&pdev->dev, sizeof(*mbox), GFP_KERNEL);
+	if (!mbox)
+		return -ENOMEM;
 
-    mbox->irq = platform_get_irq(pdev, 0);
-    if (mbox->irq < 0)
-        return dev_err_probe(&pdev->dev, mbox->irq, "Failed to get IRQ\n");
+	// Store the mailbox structure in the platform device's driver data
+	platform_set_drvdata(pdev, mbox);
+	mbox->dev = &pdev->dev;
+	rpi_mbox_global = mbox;
 
-    ret = devm_request_irq(&pdev->dev, mbox->irq, bcm2835_mbox_irq,
+	// Get the IRQ resource for the mailbox
+	mbox->irq = platform_get_irq(pdev, 0);
+	if (mbox->irq < 0) {
+		ret = dev_err_probe(&pdev->dev, mbox->irq, "Failed to get IRQ\n");
+		goto err_free_mbox;
+	}
+
+	// Request the IRQ and associate it with the mailbox IRQ handler
+	ret = devm_request_irq(&pdev->dev, mbox->irq, rpi_mbox_irq,
 			       0, dev_name(&pdev->dev), mbox);
-    if (ret)
-        return dev_err_probe(&pdev->dev, ret, "Failed to request IRQ\n");
+	if (ret) {
+		dev_err_probe(&pdev->dev, ret, "Failed to request IRQ\n");
+		goto err_free_mbox;
+	}
 
-    res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-    mbox->regs = devm_ioremap_resource(&pdev->dev, res);
-    if (IS_ERR(mbox->regs))
-        return PTR_ERR(mbox->regs);
+	// Get the memory resource for the mailbox registers
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	mbox->regs = devm_ioremap_resource(&pdev->dev, res);
+	if (IS_ERR(mbox->regs)) {
+		ret = PTR_ERR(mbox->regs);
+		dev_err(&pdev->dev, "Failed to map mailbox registers: %d\n", ret);
+		goto err_free_mbox;
+	}
 
-    mbox->controller.dev = &pdev->dev;
-    mbox->controller.chans = mbox->chans;
-    mbox->controller.num_chans = BCM2835_MAX_CHANNELS;
-    mbox->controller.ops = &bcm2835_chan_ops;
-    mbox->controller.txdone_poll = true;
-    mbox->controller.txpoll_period = 5;
+	// Initialize the mailbox controller
+	mbox->controller.dev = &pdev->dev;
+	mbox->controller.chans = mbox->chans;
+	mbox->controller.num_chans = BCM2835_MAX_CHANNELS;
+	mbox->controller.ops = &rpi_mbox_chan_ops;
+	mbox->controller.txdone_poll = true;
+	mbox->controller.txpoll_period = 5;
 
-    ret = devm_mbox_controller_register(&pdev->dev, &mbox->controller);
-    if (ret) {
-        dev_err(&pdev->dev, "Failed to register mailbox controller: %d\n", ret);
-        return ret;
-    }
+	// Register the mailbox controller
+	ret = devm_mbox_controller_register(&pdev->dev, &mbox->controller);
+	if (ret) {
+		dev_err(&pdev->dev, "Failed to register mailbox controller: %d\n", ret);
+		goto err_free_mbox;
+	}
 
-    return 0;
+	// Log successful initialization
+	dev_info(&pdev->dev, "rpi-mailbox device initialized successfully\n");
+	return 0;
+
+err_free_mbox:
+	// Cleanup in case of errors
+	devm_kfree(&pdev->dev, mbox);
+	return ret;
 }
 
-static const struct acpi_device_id bcm2835_mbox_acpi_ids[] = {
-    { "BCM2849", 0 },
-    { }
+static const struct acpi_device_id rpi_mbox_acpi_ids[] = {
+	{ "BCM2849", 0 },
+	{ }
 };
-MODULE_DEVICE_TABLE(acpi, bcm2835_mbox_acpi_ids);
+MODULE_DEVICE_TABLE(acpi, rpi_mbox_acpi_ids);
 
-static struct platform_driver bcm2835_mbox_driver = {
-    .driver = {
-        .name = "bcm2835-mbox-acpi",
-        .acpi_match_table = bcm2835_mbox_acpi_ids,
-    },
-    .probe = bcm2835_mbox_probe,
+static struct platform_driver rpi_mbox_driver = {
+	.driver = {
+		.name = "rpi-mbox",
+		.acpi_match_table = rpi_mbox_acpi_ids,
+	},
+	.probe = rpi_mbox_probe,
 };
 
-module_platform_driver(bcm2835_mbox_driver);
+module_platform_driver(rpi_mbox_driver);
 
-MODULE_AUTHOR("Richard Jeans");
-MODULE_DESCRIPTION("BCM2835 ACPI mailbox controller");
-MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Richard Jeans <rich@jeansy.org>");
+MODULE_DESCRIPTION("ACPI adaptation of the BCM2835 mailbox controller");
+MODULE_LICENSE("GPL v2");
--- drivers/acpi/rpi/rpi-mailbox.h
+++ drivers/acpi/rpi/rpi-mailbox.h
@@ -1,50 +1,19 @@
 // SPDX-License-Identifier: GPL-2.0
-#ifndef MAILBOX_BCM2835_ACPI_H
-#define MAILBOX_BCM2835_ACPI_H
+#ifndef RPI_MAILBOX_H
+#define RPI_MAILBOX_H
 
-#include <linux/mailbox_client.h>
 #include <linux/mailbox_controller.h>
-#include <linux/platform_device.h>
+
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-extern struct mbox_chan *bcm2835_mbox_request_channel(struct mbox_client *);
-extern int bcm2835_mbox_free_channel(struct mbox_chan *);
-extern struct mbox_chan * bcm2835_mbox_request_firmware_channel(struct mbox_client *);
+extern struct mbox_chan *rpi_mbox_request_channel(struct mbox_client *);
+extern int rpi_mbox_free_channel(struct mbox_chan *);
+extern struct mbox_chan *rpi_mbox_request_firmware_channel(struct mbox_client *);
 
-struct rpi_firmware_power_msg {
-	u32 size;           // Total size of the buffer in bytes
-	u32 code;           // Request code (0 = process request)
 
-	struct {
-		u32 tag;        // Tag ID (0x00030001 = set power state)
-		u32 buf_size;   // Size of the value buffer (8)
-		u32 val_len;    // Length of the actual value data (8)
-		u32 domain;     // Power domain ID (e.g., 0x00000000 = SD card)
-		u32 state;      // Bit 0: 1 = ON, Bit 1: 1 = WAIT
-		u32 padding;
-	} __packed body;
-
-	u32 end_tag;        // 0
-} __packed;
-
-struct rpi_firmware_clock_msg {
-	u32 size;      // Total size of the message in bytes
-	u32 code;      // Request code: 0 = request
-
-	struct {
-		u32 tag;        // ???? = set clock state
-		u32 buf_size;   // Size of the value buffer: 8
-		u32 val_len;    // Length of value data: 8
-		u32 clock_id;   // ID of the clock (e.g. 4 = PWM)
-		u32 state;      // Bit 0: 1=enable, Bit 1: 1=wait
-		u32 padding;    // Padding for alignment
-	} __packed body;
-
-	u32 end_tag;   // Always 0
-} __packed;
 
 
 
@@ -52,4 +21,4 @@ struct rpi_firmware_clock_msg {
 }
 #endif
 
-#endif // MAILBOX_BCM2835_ACPI_H
+#endif // RPI_MAILBOX_H
