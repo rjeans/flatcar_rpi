2f9743e8159d8783986f243e3d0121b43691f577
Richard Jeans <rich@jeansy.org>
2025-06-07 14:03:31 +0200

    up

--- rpi-acpi-thermal.c
+++ rpi-acpi-thermal.c
@@ -148,32 +148,7 @@ static acpi_handle find_cooling_device_handle(struct device *dev, acpi_handle pa
 }
 
 
-	/* Additional diagnostics for result handle */
-	char path[ACPI_PATHNAME_MAX];
-	struct acpi_buffer path_buf = { .length = sizeof(path), .pointer = path };
-	status = acpi_get_name(result, ACPI_FULL_PATHNAME, &path_buf);
-	if (ACPI_SUCCESS(status)) {
-		dev_info(dev, "Cooling-device ACPI path: %s\n", path);
-	} else {
-		dev_warn(dev, "Failed to get ACPI path for cooling-device: %s\n",
-		         acpi_format_exception(status));
-	}
 
-	struct acpi_device *adev;
-	if (acpi_bus_get_device(result, &adev)) {
-		dev_warn(dev, "Cooling-device ACPI bus lookup failed\n");
-	} else {
-		dev_info(dev, "Cooling-device found: %s\n", dev_name(&adev->dev));
-		dev_info(dev, "Cooling-device driver: %s\n",
-		         adev->dev.driver ? adev->dev.driver->name : "none");
-		if (!adev->dev.driver_data)
-			dev_warn(dev, "Cooling-device driver_data is NULL\n");
-	}
-
-out:
-	kfree(buf.pointer);
-	return result;
-}
 
 
 static struct thermal_zone_device_ops rpi_acpi_thermal_ops = {
